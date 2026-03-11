32ca54ae7dcd94d253db0ea02aea134a780d77fb
ChittaboyinaPooja <chpooja1405@gmail.com>
2026-03-11 11:50:19 +0530

    Update Reg.jsx

--- src/Reg.jsx
+++ src/Reg.jsx
@@ -2,54 +2,55 @@ import React, { useState } from "react";
 import axios from "axios";
 
 function Reg() {
-  const [data, setData] = useState({
-    name: "",
-    email: "",
-  });
-//   const changeName=(e)=>
-//   {
-//     setData({...data,[e.target.name]:e.target.value})
-//   }
 
-  const submit = async () => {
-    try {
-      const res = await axios.post(
-        "http://cabsystemsms-1.onrender.com/register",
-        data
-      );
-      alert(res.data);
-    } catch (xyz) {
-      alert(xyz.message);
+const [data, setData] = useState({
+name: "",
+email: ""
+});
+
+const submit = async () => {
+try {
+const res = await axios.post(
+"https://cabsystemsms-1.onrender.com/register",
+data
+);
+alert(res.data);
+} catch (error) {
+alert("Error: " + error.message);
+}
+};
+
+return ( <div> <h1>Registration Page</h1>
+
+```
+  <input
+    type="text"
+    placeholder="Enter Name"
+    value={data.name}
+    onChange={(e) =>
+      setData({ ...data, name: e.target.value })
     }
-  };
+  />
 
-  return (
-    <div>
-      <h1>Registration Page</h1>
+  <br /><br />
 
-      <input
-        type="text"
-        placeholder="Enter Name"
-        value={data.name}
-        onChange={(e) =>
-          setData({ ...data, name: e.target.value })
-        }
-      />
-      <br />
+  <input
+    type="email"
+    placeholder="Enter Email"
+    value={data.email}
+    onChange={(e) =>
+      setData({ ...data, email: e.target.value })
+    }
+  />
 
-      <input
-        type="email"
-        placeholder="Enter Email"
-        value={data.email}
-        onChange={(e) =>
-          setData({ ...data, email: e.target.value })
-        }
-      />
-      <br />
+  <br /><br />
 
-      <button onClick={submit}>Submit</button>
-    </div>
-  );
+  <button onClick={submit}>Submit</button>
+</div>
+```
+
+);
 }
 
 export default Reg;
+
